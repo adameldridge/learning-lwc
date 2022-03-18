c3ccfe6f7559281f4d11d624a10d33106d5bba04
Adam Eldridge <adam.eldridge@gentrack.com>
2022-03-18 19:02:37 +0000

    two data binding

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+    fullName="Zero to Hero";
+    title = "aura";
+
+    changeHandler(event){
+        this.title = event.target.value;
+    }
+
+}
